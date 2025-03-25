3387523db95fe266ff9a12d2fe82bf0fe0bfc870
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2025-03-25 13:22:34 +0100

    started writing the convert function, checking if the literal is empty and converting it already if it is a char

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,46 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 00:39:25 by dpaluszk          #+#    #+#             */
-/*   Updated: 2025/03/25 00:39:26 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2025/03/25 13:21:58 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(void)
+{
+}
+
+ScalarConverter::ScalarConverter(ScalarConverter const &copy)
+{
+	*this = copy;
+}
+
+ScalarConverter &ScalarConverter::operator=(ScalarConverter const &copy)
+{
+	(void)copy;
+	return (*this);
+}
+
+ScalarConverter::~ScalarConverter(void)
+{
+}
+
+void ScalarConverter::convert(const std::string &literal)
+{
+	if (literal.empty())
+	{
+		std::cout << "The literal input is empty." << std::endl;
+		return;
+	}
+
+	if (literal.length() == 1 && std::isprint(literal[0]) && !std::isdigit(literal[0]))
+	{
+		char c = literal[0];
+		std::cout << "The literal input is a char: '" << c << "'" << std::endl;
+		std::cout << "As int: " << static_cast<int>(c) << std::endl;
+		std::cout << "As float: " << static_cast<float>(c) << ".0f" << std::endl;
+		std::cout << "As double: " << static_cast<double>(c) << ".0" << std::endl;
+		return;
+	}
+}
