592ae1b6977cf0498e245d351e2f825ab6e9e406
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2025-03-28 15:33:43 +0100

    main slightly modified

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 15:17:42 by dpaluszk          #+#    #+#             */
-/*   Updated: 2025/03/28 14:16:13 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2025/03/28 15:33:28 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,20 @@ int main(void)
 	uintptr_t raw = Serializer::serialize(&original);
 	Data *ptr = Serializer::deserialize(raw);
 
+	std::cout << std::endl;
 	std::cout << "Original address: " << &original << std::endl;
 	std::cout << "Serialized address: " << raw << std::endl;
 	std::cout << "Deserialized address: " << ptr << std::endl;
+	std::cout << std::endl;
 	std::cout << "Original index: " << original.index << std::endl;
 	std::cout << "Deserialized index: " << ptr->index << std::endl;
+	std::cout << std::endl;
 	std::cout << "Original message: " << original.message << std::endl;
 	std::cout << "Deserialized message: " << ptr->message << std::endl;
+	std::cout << std::endl;
 	std::cout << "Original other_value: " << original.other_value << std::endl;
 	std::cout << "Deserialized other_value: " << ptr->other_value << std::endl;
+	std::cout << std::endl;
 
 	return (0);
 }
