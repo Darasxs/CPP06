3038413f71d1b312cd4de0f7afd595d9aaffdf03
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2025-03-29 14:43:50 +0100

    added two identify functions

--- ex02/mandatoryFunctions.cpp
+++ ex02/mandatoryFunctions.cpp
@@ -6,20 +6,57 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:51:45 by dpaluszk          #+#    #+#             */
-/*   Updated: 2025/03/28 16:57:11 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2025/03/29 14:43:27 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mandatoryFunctions.hpp"
 
-Base *generate(void)
+Base	*generate(void)
 {
-	int i = rand() % 3;
-	if(i == 0)
-		return new A();
-	else if(i == 1)
-		return new B();
+	int	i;
+
+	i = rand() % 3;
+	if (i == 0)
+		return (new A());
+	else if (i == 1)
+		return (new B());
 	else
-		return new C();
+		return (new C());
 }
 
+void	identify(Base *p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "Class A object." << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "Class B object." << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "Class C object." << std::endl;
+	else
+		std::cout << "Unknown" << std::endl;
+}
+
+void	identify(Base &p)
+{
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "Class A object." << std::endl;
+	}
+	catch (std::bad_cast &){}
+	
+	try
+	{
+		(void)dynamic_cast<B&>(p);
+		std::cout << "Class B object." << std::endl;
+	}
+	catch (std::bad_cast &) {}
+	
+	try
+	{
+		(void)dynamic_cast<C&>(p);
+		std::cout << "Class C object." << std::endl;
+	}
+	catch (std::bad_cast &) {}
+}
